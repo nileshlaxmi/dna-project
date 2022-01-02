0e61a40d8b82b0156c638abb37fb5edc3790ea5e
nileshlaxmi <nileshlaxmi4@gmail.com>
2022-01-03 04:51:08 +0530

    fixed missing file issue

--- src/assets/iconsComponent/index.js
+++ src/assets/iconsComponent/index.js
@@ -1,27 +1,8 @@
-export { default as DashboardIcon } from './leftNavBarIcons/DashboardIcon';
-export { default as UsersIcon } from './leftNavBarIcons/UsersIcon';
-export { default as EmployeesIcon } from './leftNavBarIcons/EmployeesIcon';
-export { default as BulkUploadIcon } from './leftNavBarIcons/BulkUploadIcon';
-export { default as ShiftIcon } from './leftNavBarIcons/ShiftIcon';
-export { default as SettingsIcon } from './leftNavBarIcons/SettingsIcon';
-export { default as ScreenlockIcon } from './leftNavBarIcons/ScreenlockIcon';
-
-export { default as EditIcon } from './EditIcon';
 export { default as ChevronUp } from './ChevronUp';
 export { default as ChevronDown } from './ChevronDown';
-export { default as Deboard } from './Deboard';
 export { default as Arrow } from './Arrow';
-export { default as AddEmp } from './AddEmp';
-export { default as InviteUserIcon } from './InviteUser';
 export { default as Redirect } from './Redirect';
 
 export { default as AccordionDownIcon } from './AccordionDownIcon';
 export { default as AccordionLeftIcon } from './AccordionLeftIcon';
-export { default as CalendarIcon } from './Calendar';
-export { default as ExemptIcon } from './Exempt';
-export { default as RetainIcon } from './Retain';
-export { default as DownloadIcon } from './Download';
-export { default as DownloadFileIcon } from './DownloadFile';
 
-export { default as PlayIcon } from './Play';
-export { default as PauseIcon } from './Pause';
